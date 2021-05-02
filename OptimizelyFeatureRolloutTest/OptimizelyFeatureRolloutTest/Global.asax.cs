48defb33df50a99152160ffe1db2b4b07b3b3eb6
Giuliano Dore <giuliano.dore@dotcentric.co.uk>
2021-05-02 15:34:49 +0100

    sdk config, optimizely client and static prop to check feature

--- OptimizelyFeatureRolloutTest/OptimizelyFeatureRolloutTest/Global.asax.cs
+++ OptimizelyFeatureRolloutTest/OptimizelyFeatureRolloutTest/Global.asax.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using OptimizelySDK;
+using System.Configuration;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -10,12 +8,20 @@ namespace OptimizelyFeatureRolloutTest
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static bool EnableMessengerChat { get; private set; }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //setting up Optimizely
+            var optimizelyClient = OptimizelyFactory.NewDefaultInstance(ConfigurationManager.AppSettings["Optimizely:sdkKey"]);
+            
+            //for the sake of the demo we will test for all users
+            EnableMessengerChat = optimizelyClient.IsFeatureEnabled("enable_messenger_chat", "");
         }
     }
 }
